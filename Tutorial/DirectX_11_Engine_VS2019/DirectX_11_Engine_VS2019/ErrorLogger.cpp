f9b1aba83d4ef8064b96c5cd0761af5e52cad9e5
Damian Andrysiak <andrydamian@gmail.com>
2022-09-10 22:03:13 +0200

    add Tutorial folder with basic project

---
+++ Tutorial/DirectX_11_Engine_VS2019/DirectX_11_Engine_VS2019/ErrorLogger.cpp
@@ -0,0 +1,18 @@
+#include "ErrorLogger.h"
+#include <comdef.h>
+
+#include <utility>
+
+void ErrorLogger::Log(std::string message)
+{
+	const std::string error_message = "Error: " + message;
+	MessageBoxA(nullptr, error_message.c_str(), "Error", MB_ICONERROR);
+}
+
+void ErrorLogger::Log(HRESULT hr, std::string message)
+{
+	_com_error error(hr);
+	//error.ErrorMessage();
+	const std::wstring error_message = L"Error: " + StringConverter::StringToWide(std::move(message)) + L"\n" + error.ErrorMessage();
+	MessageBoxW(nullptr, error_message.c_str(), L"Error", MB_ICONERROR);
+}
